70019e4ca0fca8f97360e6700946df8fd5130ae1
jahidul-arafat <jahidapon@gmail.com>
2024-01-08 22:46:32 +0600

    loop unrolling

--- JavaAdvHandsonStreamLambdaCollectionGenerics/src/main/java/org/example/playground/map_examples/Main.java
+++ JavaAdvHandsonStreamLambdaCollectionGenerics/src/main/java/org/example/playground/map_examples/Main.java
@@ -1,4 +1,70 @@
 package org.example.playground.map_examples;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static void main(String[] args) {
+        // define a list of integers from 1 to 10
+        Integer[] intArr = {1,2,3,4,5,6,7,8,9,10};
+        List<Integer> intList = new ArrayList<>(Arrays.asList(intArr));
+
+        // Define a BiFunction Function which takes two arguments and returns a value multiple by N
+        BiFunction<Integer, Integer,Integer> mulN = (i,n)->i*n;
+        Function<Integer,Double> times2 = (i)->i*2.0;
+
+        var doubledValue = intList.stream()
+                .map(item->mulN.apply(item,2))
+                .collect(Collectors.toList());
+        System.out.println(doubledValue);
+
+        var doubledValue2 = intList.stream()
+                .map(value-> 2*value.doubleValue())
+                        .reduce(0.0, Double::sum);
+        System.out.println(doubledValue2);
+
+        // use stream API to get the summation of all values in the intList
+        var sum = intList.stream()
+                       .reduce(0, Integer::sum);
+        System.out.println(sum);
+
+        var doubleList = intList.stream()
+                .map(Integer::doubleValue)
+                .toList();
+        System.out.println(doubleList);
+
+        var intList2 = doubleList.stream()
+                .map(Double::intValue)
+                .collect(Collectors.toList());
+        System.out.println(intList2);
+
+        String[] intStrArr={"1.2","2","3","4","5","6","7","8","9","10"};
+        List<String> intStrList = new ArrayList<>(Arrays.asList(intStrArr));
+        var intStrListInt = intStrList.stream()
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+        System.out.println(intStrListInt);
+
+        var sumDouble = intStrListInt.stream()//Double::doubleValue is a method reference to Double class; referring the method "doubleValue"
+                        .reduce(0.0, Double::sum);
+        System.out.println(sumDouble);
+
+        // By Jahidul Arafat, Solution Architect (AppDev and DevOps)
+        // Clean Code Writer
+        Function<Integer,Function<Integer,Integer>> timesFactory = (item)->times->item*times;
+        var timesFactory2 = timesFactory.apply(2);
+        var timesFactory3 = timesFactory.apply(3);
+
+        var intListTimes3 = intList.stream()
+                .map(timesFactory3::apply)
+                .collect(Collectors.toList());
+        System.out.println(intListTimes3);
+
+
+
+    }
 }
--- JavaAdvHandsonStreamLambdaCollectionGenerics/src/main/java/org/example/playground/map_examples/Playground.java
+++ JavaAdvHandsonStreamLambdaCollectionGenerics/src/main/java/org/example/playground/map_examples/Playground.java
@@ -1,4 +1,44 @@
 package org.example.playground.map_examples;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Playground {
+    public static void main(String[] args) {
+        // an array of integers
+        // int-> primitive data type-> Default value is O -> non-nullable
+        // Integer -> Object Data type -> is a wrapper class for primitive data type 'int' -> Default value is 'null' -> nullable -> additional memory overhead:: cost of unboxing
+        Integer[] numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> numList = new ArrayList<>(Arrays.asList(numArray)); // mutable List
+        numList.add(100); // mutable List
+        System.out.println(numList);
+
+        // Example: Loop Unrolling
+        // Question: As a Java Developer, do you really need to explicitly request Loop Unrolling? -> NO
+        // Solution: JVM is responsible for performance optimization, including loop unrolling, during JIT compilation phase.
+        // JVM makes this decision based on the runtime profiling information. So Loop unrolling doesnt need to be explicitly mentioned, JVM does that part for you.
+        /*
+        Benefits:
+        - Reduce Loop Control Overhead
+        - Improve Instruction-Level Parallelism
+
+        Challenges
+        - Tradeoff between Reduced Loop Control Overhead + improved performance VS increases the size of the compiled code.
+
+         */
+        // define an empty array of size 10
+        Integer[] intArray = new Integer[10];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = i;
+        }
+        System.out.println(Arrays.asList(intArray));
+
+        Integer[] intArrayUnrolling = new Integer[10];
+        for (int i = 0; i < intArrayUnrolling.length; i+=2) {
+            intArrayUnrolling[i] = i;
+            intArrayUnrolling[i+1] = i+1; // Loop Unrolling
+        }
+        System.out.println(Arrays.asList(intArrayUnrolling));
+    }
 }
